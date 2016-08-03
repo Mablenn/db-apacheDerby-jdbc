123d729d941c1a8543a2952285d0de44759482d8
mablens <marcos.broseta@gmail.com>
2016-08-03 10:17:06 +0200

    Añadidos nuevos archivos de ejemplo

--- Ejemplo_04_conexionDB_delete.java
+++ Ejemplo_04_conexionDB_delete.java
@@ -1,8 +1,6 @@
 /*
  * Clase que establece una conexión a una base de datos 
- * y ejecuta una consulta sin resultados (UPDATE, INSERT, DELETE)
- * La clase Ejemplo_01_conexionDB muestra como establecer la conexión, ahora utilizaremos 
- * un objeto de la clase Statemant para establecer sentencias y ejecutar consultas SQL.
+ * y ejecuta una consulta sql para borrar registros de la BBDD
  * 
  * Clases y métodos a destacar
  * Class.forName() -> Clase encargada de cargar el controlador en memoria
@@ -10,7 +8,7 @@
  * DriverManager.getConnection() -> Método que establece la conexión
  * Statement -> Clase para trabajar con sentencias sobre una BBDD
  * createStatement() -> Método de la clase Statement que crea el statement (sentencia)
- * exectue() -> Método de la clase Statement que lanzará las cnosultas SQL
+ * execute() -> Método de la clase Statement que lanzará las cnosultas SQL
  * 
  * Excepciones:
  * ClassNotFoundException -> Lanzada por la clase 'Class"
@@ -23,12 +21,14 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.sql.Statement;
 
-public class Ejemplo_02_conexionDB_insert {
+
+public class Ejemplo_04_conexionDB_delete {
     //Controlador y URL de la BBDD
     static final String DRIVER = ("org.apache.derby.jdbc.EmbeddedDriver"); 
     static final String URL = ("jdbc:derby://localhost:1527/ejemploDB;create=true");
     
     public static void main(String[] args){
+        
         //Objeto Connection para establecer la conexión
         Connection cn = null;
         //Objeto Statement para crear sentencias y realizar consultas SQL sobre la BBDD
@@ -38,15 +38,15 @@ public class Ejemplo_02_conexionDB_insert {
             //Carga del controlador en memoria
             Class.forName(DRIVER);                 
             //Establece conexion
-            cn = DriverManager.getConnection(URL);  
+            cn = DriverManager.getConnection( URL);  
             //Crear objeto Statemennt(sentencia) para enviar consultas a la BBDD.
             st = cn.createStatement();          
             //Ejecutar sentencias SQL
             //El método 'execute' de la clase Statemant solo permite realizar consultas SQL
             //sin resultados, como pueden ser INSERT, UPDATE, DELETE.
-            st.execute("INSERT INTO MUESTRA(ID, CLIENTE) "
-                    + "VALUES (254,'Felipe Almeida'),(443, 'Eusebio Sanchez'), (9874, 'Eduardo Vazquez')");
-            System.out.println("Registros añadidos en la BBDD");
+            st.execute("DELETE FROM MUESTRA WHERE ID = 254");
+            System.out.println("Registro borrado de la tabla");
+            
             //Cierre del Statement(sentencia)
             st.close();
             //Cierre de la cnexion
@@ -54,5 +54,5 @@ public class Ejemplo_02_conexionDB_insert {
         }
         catch(ClassNotFoundException ex){ System.out.println(ex.getMessage()); }
         catch(SQLException ex){ System.out.println(ex.getMessage()); }
-    } //Fin main
-} //Fin clase
+    }
+}
